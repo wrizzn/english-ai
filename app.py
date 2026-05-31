056a996cae1effec9ecdc09e552344a28a33c47b
Pannacott_ <140075632+wrizzn@users.noreply.github.com>
2026-05-31 14:59:30 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,21 +8,23 @@ st.title("🤖 AI ตรวจภาษาอังกฤษ (CEFR Evaluator)")
 st.write("ระบบวิเคราะห์ระดับภาษาและไวยากรณ์สำหรับนักเรียนมัธยม")
 
 # สร้างช่องให้กรอก API Key ผ่านหน้าเว็บ (เพื่อความปลอดภัย จะได้ไม่ต้องฝังในโค้ด)
-api_key_input = st.text_input("ใส่รหัส API Key ของคุณเพื่อเริ่มใช้งาน:", type="password")
+SECRET_API_KEY = st.secrets["GEMINI_API_KEY"]
+#api_key_input = st.text_input("ใส่รหัส API Key ของคุณเพื่อเริ่มใช้งาน:", type="password")
 
 # กล่องข้อความให้นักเรียนพิมพ์
 student_text = st.text_area("พิมพ์ประโยคภาษาอังกฤษของคุณที่นี่:", "I has two dog, they is very cute.")
 
 # 2. ลอจิกเมื่อกดปุ่ม "ส่งคำตอบ"
 if st.button("ส่งให้ AI ตรวจ"):
-    if not api_key_input:
-        st.warning("⚠️ กรุณาใส่ API Key ด้านบนก่อนครับ")
-    else:
+    # if not api_key_input:
+    #     st.warning("⚠️ กรุณาใส่ API Key ด้านบนก่อนครับ")
+    # else:
         # แสดงแอนิเมชันหมุนๆ ระหว่างรอ AI คิด
         with st.spinner('กำลังวิเคราะห์โครงสร้างประโยค...'):
             try:
                 # โค้ดเชื่อมต่อ API (เหมือนที่คุณเพิ่งรันผ่าน)
-                client = genai.Client(api_key=api_key_input)
+                client = genai.Client(api_key=SECRET_API_KEY)
+                #client = genai.Client(api_key=api_key_input)
                 system_prompt = """
                 วิเคราะห์ประโยคภาษาอังกฤษของนักเรียนมัธยม ส่งคืนผลลัพธ์เป็น JSON
                 รูปแบบ:
